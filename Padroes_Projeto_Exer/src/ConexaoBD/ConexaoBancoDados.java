28781f70324af36c0ff66923828a9ba9102324af
Diego <Diego@192.168.1.7>
2020-05-01 15:18:25 -0300

    Commit Conexão com BD

---
+++ Padroes_Projeto_Exer/src/ConexaoBD/ConexaoBancoDados.java
@@ -0,0 +1,32 @@
+package ConexaoBD;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
+public class ConexaoBancoDados {
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost:3306/test";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "admin";
+   
+    public static Connection getConexao() throws SQLException{
+        try{
+            Class.forName(DRIVER);
+                return DriverManager.getConnection(URL, USUARIO, SENHA);
+        }catch(ClassNotFoundException cnfex){
+            throw new SQLDataException(cnfex.getMessage());
+        }
+    }
+    
+    public static void main(String[] args){
+        try{
+            getConexao();
+            System.out.println("Banco de dados conectado com sucesso!");
+        }catch(SQLException sqlex){
+            System.out.println("Impossível se conectar ao banco de dados! "
+                    + "ERRO: " + sqlex.getMessage());
+        }
+    }
+}
